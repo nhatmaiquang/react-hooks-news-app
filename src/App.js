821a2b3bcab161394411dba85cc5b9e924f49c40
Mai Quang Nhat <quangnhat224@gmail.com>
2020-05-03 11:16:38 +0300

    loading state and reorganize code

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { Component, useState, useEffect } from 'react';
 
 const App = () => {
   // state
-  const [news, setNews] = useState([])
+  const [news, setNews] = useState([]);
   const [seachQuery, setSearchQuery] = useState('react');
-  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react')
+  const [url, setUrl] = useState(
+    'http://hn.algolia.com/api/v1/search?query=react'
+  );
+  const [loading, setLoading] = useState(false);
   // fetch news
   const fetchNews = () => {
+    // set loading true
+    setLoading(true)
     fetch(url)
       .then(result => result.json())
       // .then(data => console.log(data))
-      .then(data => setNews(data.hits))
+      .then(data => (setNews(data.hits), setLoading(false)))
       .catch(error => console.log(error));
   };
 
@@ -20,33 +25,36 @@ const App = () => {
 
   const handleChange = (e) => {
     setSearchQuery(e.target.value)
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault()
     setUrl(`http://hn.algolia.com/api/v1/search?query=${seachQuery}`)
-  }
+  };
+
+  const showLoading = () => (
+    loading ? <h2>Loading...</h2> : ""
+  );
+
+  const searchForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={seachQuery} onChange={handleChange} />
+      <button>Search</button>
+    </form>
+  );
+
+  const showNews = () => (news.map((n, i) => (
+    <p key={i}>{n.title}</p>
+  )));
 
   return (
     <div>
       <h2>News</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={seachQuery} onChange={handleChange} />
-        <button>Search</button>
-      </form>
-      {news.map((n, i) => (
-        <p key={i}>{n.title}</p>
-      ))}
+      {showLoading()}
+      {searchForm()}
+      {showNews()}
     </div>
   )
-}; 
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <h2>lon me may</h2>
-//     );
-//   }
-// }
+};
 
 export default App;
