3c23154ccc4b30047714613d4fe06f030106724b
Mai Quang Nhat <quangnhat224@gmail.com>
2020-05-03 10:46:08 +0300

    list news

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  // state
+  const [news, setNews] = useState([])
+  // fetch news
+  const fetchNews = () => {
+    fetch('http://hn.algolia.com/api/v1/search?query=react')
+      .then(result => result.json())
+      // .then(data => console.log(data))
+      .then(data => setNews(data.hits))
+      .catch(error => console.log(error));
+  };
+  useEffect(() => {
+    fetchNews()
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>News</h2>
+      {news.map((n, i) => (
+        <p key={i}>{n.title}</p>
+      ))}
     </div>
-  );
-}
+  )
+};
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <h2>lon me may</h2>
+//     );
+//   }
+// }
 
 export default App;
